c58b8e30d2b2f1d2cb3d48458d2c0eaf7c55ad93
15faiq <maulanafaiq85@gmail.com>
2021-03-02 17:21:12 +0700

    tugas_4

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,86 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'convert.dart';
+import 'input.dart';
+import 'result.dart';
+import 'riwayat.dart';
 
-void main() {
-  runApp(MyApp());
+void main() => runApp(Suhu());
+
+class Suhu extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => MainSuhu();
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MainSuhu extends State<Suhu> {
+  final SuhuController = TextEditingController();
+  double _inputSuhu = 0;
+  String _valueDropDown = "Kelvin";
+  double _result = 0;
+
+  List<String> ListString = ["Kelvin", "Reamur", "Fahrenheit"];
+  // ignore: deprecated_member_use
+  List<String> listViewItem = List<String>();
+
+  void rumusSuhu() => setState(() {
+        _inputSuhu = double.parse(SuhuController.text);
+        if (_valueDropDown == "Kelvin")
+          _result = _inputSuhu + 273;
+        else if (_valueDropDown == "Reamur")
+          _result = (4 / 5) * _inputSuhu;
+        else
+          _result = (_inputSuhu * 9 / 5) + 32;
+        listViewItem.add("$_valueDropDown : $_result");
+      });
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Konverter Suhu"),
+        ),
+        body: Container(
+          margin: EdgeInsets.all(8),
+          child: Column(
+            children: [
+              Input(SuhuController: SuhuController),
+              DropdownButton<String>(
+                items: ListString.map((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                value: _valueDropDown,
+                onChanged: (String changeValue) {
+                  setState(() {
+                    _valueDropDown = changeValue;
+                    rumusSuhu();
+                  });
+                },
+              ),
+              Result(result: _result),
+              Convert(convertHandler: rumusSuhu),
+              Container(
+                margin: EdgeInsets.only(top: 10, bottom: 10),
+                child: Text(
+                  "Riwayat Konversi",
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+              Expanded(
+                child: Riwayat(listViewItem: listViewItem),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
